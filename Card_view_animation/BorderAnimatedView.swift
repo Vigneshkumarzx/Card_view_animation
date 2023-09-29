1596ecd4a0cbbcd7c79c9c56e3f2971c943b6497
Vigneshkumar <vigneshzx@gmail.com>
2023-09-29 16:25:56 +0530

    border animation added

---
+++ Card_view_animation/BorderAnimatedView.swift
@@ -0,0 +1,18 @@
+//
+//  BorderAnimatedView.swift
+//  Card_view_animation
+//
+//  Created by vignesh kumar c on 29/09/23.
+//
+
+import SwiftUI
+
+struct BorderAnimatedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BorderAnimatedView()
+}
